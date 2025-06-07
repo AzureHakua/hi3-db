cd447f5f7f872e498341406c66659f008572aa5e
Hakua <azureshockwave@gmail.com>
2025-06-06 23:13:58 -0400

    updated stigmata images on mobile; updated packages

--- src/index.tsx
+++ src/index.tsx
@@ -110,10 +110,10 @@ app.get('/stigmata/:id/position/:index', async ({ params }) => {
   return (
     <div>
       {stigma.images && stigma.images.length > 0 && (
-        <div class="rounded-none border-2 border-slate-400 overflow-hidden aspect-square mx-10 hidden md:flex">
+        <div class="rounded-none border-2 border-slate-400 overflow-hidden aspect-square mx-4 md:mx-10 flex">
           <img src={stigma.images.find(img => img.position === pos.position)?.imgUrl ?? ''}
             alt={`${stigma.name} ${pos.position}`}
-            class="object-cover rounded"
+            class="object-cover rounded w-full h-full"
             loading="lazy" 
           />
         </div>
